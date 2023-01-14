e42c527a62e98a7e00648ae35f41cca1fa71e050
aronovbo <aronovbo@gmail.com>
2023-01-14 18:48:58 +0300

    cycles

--- nested cycle.py
+++ nested cycle.py
@@ -21,19 +21,19 @@
 # 0 1 2 3 4
 # 0 1 2 3 4
 
-for i in range(3):
-    for j in range(i):
-        print(j, end=' ')
-    print()
+# for i in range(3):
+#     for j in range(i):
+#         print(j, end=' ')
+#     print()
 
 
 # 0
 # 0 1
 
-for i in range(1, 10):
-    for j in range(1, i+1):
-        print(j, end=' ')
-    print()
+# for i in range(1, 10):
+#     for j in range(1, i+1):
+#         print(j, end=' ')
+#     print()
 
 # 0
 # 0 1
@@ -47,3 +47,34 @@ for i in range(1, 10):
 # 1 2 3 4 5 6 7 8
 # 1 2 3 4 5 6 7 8 9
 
+# for i in range(1, 3):
+#     for j in range(20, 22):
+#         print(i,j)
+
+# 1 20
+# 1 21
+# 2 20
+# 2 21
+
+# for i in range(1, 3):
+#     for j in range(20, 22):
+#         print(j,i)
+
+# 20 1
+# 21 1
+# 20 2
+# 21 2
+
+# for i in 'ab':
+#     for j in 'cde':
+#         print(i,j)
+# a c
+# a d
+# a e
+# b c
+# b d
+# b e
+
+for i in 'ab':
+    for j in 'cde':
+        print(j,i)
