caa472bee798fc9319a48a52fbf5106a36605d65
b.aronov <aronovbo@gmail.com>
2023-01-18 15:02:25 +0300

    full

--- Заполнение змейкой.py
+++ Заполнение змейкой.py
@@ -1,15 +1,3 @@
-#
-# n, m = map(int, input().split())
-# a = []
-# for i in range(n):
-#     if i % 2 != 0:
-#         a.append([str(j + i*m).ljust(2) for j in range(m)])
-#         a[i].reverse()
-#     else:
-#         a.append([str(j + i*m).ljust(2) for j in range(m)])
-#     print(*a[i])
-
-
 n, m = map(int, input().split())
 a = []
 for i in range(n):
