38a0a1badcd1b6741a9c5d5c00aa85e1d86b602a
aronovbo <aronovbo@gmail.com>
2023-01-21 20:36:36 +0300

    cycles

--- Спираль.py
+++ Спираль.py
@@ -1,6 +1,34 @@
 n = int(input())
-a = []
-for i in range(n):
-    a.append([((j+1) ** 2) for j in range(n)])
-    break
-print(a)
+
+a = [[0] * n for i in range(n)]
+i = 1
+x = 0
+y = -1
+mr = 0
+mc = 1
+l = len(str(n ** 2))
+while i <= n ** 2:
+    if 0 <= x+mr < n  and 0 <= y + mc < n and a[x+mr][y+mc] == 0:
+        x += mr
+        y += mc
+        a[x][y] = i
+        i += 1
+    else:
+        if mc == 1:
+            mc = 0
+            mr = 1
+        elif mr == 1:
+            mr = 0
+            mc = -1
+        elif mc == -1:
+            mr = 0
+            mr = -1
+        elif mr == -1:
+            mr = 0
+            mc = 1
+for i in a:
+    for j in i:
+        print(str(i).rjust(l), end=' ')
+    print()
+
+
