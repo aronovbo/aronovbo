1d854b98c893b3e4b51090f78bce5e3f0324ed53
aronovbo <aronovbo@gmail.com>
2023-01-23 00:25:57 +0300

    cycles

---
+++ generation_1.py
@@ -0,0 +1,28 @@
+# import random
+#
+# a = [random.randint(-5, 5) for i in range(5)]
+# print(a)
+# b = [abs(j) for j in a]
+# print(b)
+# c = [j**2 for j in b if j % 2 == 0]
+# print(c)
+
+# bo = input().split()
+# bo = [int(i) for i in bo]
+# print(bo)
+
+# n = 4
+# m = 5
+#
+# a = [['+'*3] * m for i in range(n)]
+# for j in a:
+#     a[0][3] = 'bo'
+#     a[3][0] = 'aronov'
+#     print(j)
+
+# a = [[i, j, k] for i in 'aronov' for j in 'boris' for k in 'alex' if i == k]
+# print(a)
+
+
+a = [i**3 for i in range(1,11)]
+print(a)
