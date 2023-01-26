b709d278ee11ef83873473e949116c0a95e40d6f
b.aronov <aronovbo@gmail.com>
2023-01-26 19:42:28 +0300

    full

--- dict(cистема регистрации).py
+++ dict(cистема регистрации).py
@@ -2,6 +2,7 @@ n = int(input())
 s = []
 for i in range(1, n+1):
     s.append([i, input()])
+print(s)
 print('OK')
 n = 0
 m = 0
@@ -11,10 +12,11 @@ for i in range(1,len(s)):
         s[i][1] = s[i][1] + str(n * 1)
         print(s[i][1])
     elif s[0][1] != s[i][1]:
+
         m += 1
-        if s.count(i) == i:
+        if s[i][1] not in s:
             print('OK')
-        else:
+        elif s[i][1] in s:
             s[i][1] = s[i][1] + str(m * 1)
             print(s[i][1])
 
