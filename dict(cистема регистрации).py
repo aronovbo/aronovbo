3d7f9693b1706ef14c542b8abe54a4e2940913b6
b.aronov <aronovbo@gmail.com>
2023-01-26 16:52:48 +0300

    full

--- dict(cистема регистрации).py
+++ dict(cистема регистрации).py
@@ -1,1 +1,28 @@
 n = int(input())
+s = []
+for i in range(1, n+1):
+   s.append([i, input()])
+
+
+print(s)
+s_ = dict(s)
+print(s)
+
+
+
+
+
+# s = {}
+#
+# a = input()
+# b = input()
+# # c = input()
+# # d = input()
+#
+# s[1] = a
+# if b != a:
+#     s[2] = b
+# elif b == a:
+#     s[2] = b + '1'
+#
+# print(s)
