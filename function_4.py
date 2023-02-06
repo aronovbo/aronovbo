fe4bf62acec71c085e05fd4dfefc117b56b24b28
b.aronov <aronovbo@gmail.com>
2023-02-06 13:25:10 +0300

    full

---
+++ function_4.py
@@ -0,0 +1,10 @@
+def get_body_mass_index(x, y):
+    i = x / (y * 0.01) ** 2
+    if i < 18.5:
+        print('Недостаточная масса тела')
+    elif 18.5 <= i <= 25:
+        print('Норма')
+    else:
+        print('Избыточная масса тела')
+
+get_body_mass_index(70,170)
