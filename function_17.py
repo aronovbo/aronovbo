c269e0ac1fbf380353026adf7962c30c5484efd7
b.aronov <aronovbo@gmail.com>
2023-02-08 19:08:20 +0300

    full

--- function_17.py
+++ function_17.py
@@ -1,8 +1,22 @@
-q = [{'name': 'Bart'}, {'name': 'Lisa'}, {'name': 'Maggie'}]
-w = []
-# def format_name_list(q):
-for i in range(len(q)):
-    w.append(list(q[i])
-    print(w)
+
+
+def format_name_list(names: list[dict]) -> str:
+    w = []
+    for i in names:
+        w.append(i["name"])
+    if len(w) == 0:
+        return ''
+    elif len(w) == 1:
+        return w[0]
+    elif len(w) == 2:
+        return f'{w[0]} и {w[1]}'
+    else:
+        a = ''
+        for i in range(1, len(w) - 1):
+            a += (w[i - 1] + ', ')
+        a += f'{w[-2]} и {w[-1]}'
+        return a
+
+
 
 
