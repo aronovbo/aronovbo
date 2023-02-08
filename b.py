f4fdf65c348b39846971e1c8738657d5270a18e8
b.aronov <aronovbo@gmail.com>
2023-02-08 11:11:46 +0300

    full

--- b.py
+++ b.py
@@ -1,6 +1,5 @@
-a = [1,2,2,3,4,4,5,6]
-d = []
-for j in a:
-    if j not in d:
-        d.append(j)
-print(d)
+s = 'ppython'
+for i in range(len(s)):
+    if s.count(s[i]) == 1:
+        print(i)
+        break
--- function_15.py
+++ function_15.py
@@ -1,1 +1,10 @@
+s = 'abcabc'
 def first_unique_char(s):
+    for i in range(len(s)):
+        if s.count(s[i]) == 1:
+            print(i)
+            break
+
+    else:
+        print('-1')
+first_unique_char(s)
