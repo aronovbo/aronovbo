410be5253e9f2f67f261d0a21bd585e0c10eb199
b.aronov <aronovbo@gmail.com>
2023-02-10 09:56:58 +0300

    full

--- function_19.py
+++ function_19.py
@@ -1,5 +1,18 @@
+n = int(input())
 def factorial(n):
-    pass
+    x = 1
+    for i in range(2, n + 1):
+        x *= i
+    return x
+
+
 
 def trailing_zeros(n):
-    pass
+    w = 0
+    for i in reversed(str(factorial(n))):
+        if int(i) == 0:
+            w += 1
+        elif int(i) != 0:
+            break
+    return w
+print(trailing_zeros(n))
---
+++ function_20(DNA).py
@@ -0,0 +1,19 @@
+dna = input()
+def count_AGTC(dna):
+    d =[]
+    a = 0
+    g = 0
+    t = 0
+    c = 0
+    for i in dna:
+        if i == 'A':
+            a += 1
+        elif i == 'G':
+            g += 1
+        elif i == 'T':
+            t += 1
+        elif i == 'C':
+            c += 1
+    z = a, g, t, c
+    return z
+print(count_AGTC(dna))
