ea79e24f39d659dd22ec397bbaf4f73e7f308a6a
b.aronov <aronovbo@gmail.com>
2023-02-15 19:04:35 +0300

    full

--- annotation_1.py
+++ annotation_1.py
@@ -1,6 +1,11 @@
 letter = input()
 shift = int(input())
 def shift_letter(letter : str, shift: int):
-    s = chr(ord(letter) + shift)
-    return s
+    s = ord(letter) + shift
+    if ord(letter) + shift > 122:
+        s = 96 + shift
+        d = s - 122
+        f = 96 + d
+
+    return chr(f)
 print(shift_letter(letter,shift))
