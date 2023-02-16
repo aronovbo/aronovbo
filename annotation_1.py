24097d1ec6c906505de1a450777600609b20835b
b.aronov <aronovbo@gmail.com>
2023-02-16 14:43:56 +0300

    full

--- annotation_1.py
+++ annotation_1.py
@@ -4,14 +4,8 @@ shift = int(input())
 
 
 def shift_letter(letter : str, shift: int):
-    s = ord(letter) + shift
-    if shift > 26:
-        s = ord(letter) + shift % 26
-
-    # if ord(letter) + shift > 122:
-    #     s = 97 + ((ord(letter) + shift) % 26 - 122)
-    # elif ord(letter) + shift < 97:
-    #     s = 122 - (96 - (ord(letter) + shift))
+    'Функция сдвигает символ letter на shift позиций'
+    s = (ord(letter) - 97 + shift) % 26 + 97
 
 
     return chr(s)
